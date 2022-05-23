64957f1e09f35f8278aa761184bb32c71d96f358
Kritsanaphong <kritsanaphong-keang@hotmail.com>
2022-05-23 19:41:43 +0700

    Add project files.

---
+++ HelloWorldGUI/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorldGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float income = float.Parse(txtIncome.Text);
+                float expenses = float.Parse(txtExpenses.Text);
+                float netIncome = income - expenses;
+                if (netIncome > 0)
+                {
+                    float require = float.Parse(txtRequire.Text);
+                    string result = Math.Floor(require / netIncome).ToString();
+                    txtResult.Text = result;
+                }
+                else
+                {
+                    txtResult.Text = "";
+                }
+            } 
+            catch
+            {
+                
+            }
+        }
+    }
+}
